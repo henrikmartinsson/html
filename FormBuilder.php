97f9456d2f3ed6dbc4904798ea6d3c89528c7050
Henrik Martinsson <martinssonmeister@gmail.com>
2015-05-17 09:25:01 +0200

    Changed file upload layout

--- FormBuilder.php
+++ FormBuilder.php
@@ -239,14 +239,7 @@ class FormBuilder extends BaseFormBuilder {
             $thumbnail = $this->html->link($attachment->url());
         }
 
-        $html = '<div class="fileinput fileinput-new input-group" data-provides="fileinput">
-            <div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>';
-
-        $html .= '<span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">'.trans('admin::files.choose_file').'</span><span class="fileinput-exists">'.trans('admin::files.change').'</span>';
-
         $html .= $this->file($name, $options);
-
-        $html .= '</span><a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">'.trans('admin::files.remove').'</a></div>';
 
         /*
           Set checkbox name to the actual attribute name, even if its in a related model.
